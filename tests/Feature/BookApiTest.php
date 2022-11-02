73e01731eea4bcde399c1186b3e221e05438daa3
Fede Rivero <federicorivero18@gmail.com>
2022-11-02 04:34:49 -0300

    finished api

---
+++ tests/Feature/BookApiTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\AssertableJsonString;
+use Tests\TestCase;
+
+class BookApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function can_get_all_books()
+    {
+        $books = Book::factory(4)->create();
+
+        $response = $this->getJson(route('books.index'));
+
+        $response->assertJsonFragment([
+            'title' => $books[0]->title,
+        ])->assertJsonFragment([
+            'title' => $books[1]->title,
+        ]);
+    }
+
+    /** @test */
+    function can_get_one_book()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->getJson(route('books.show', $book));
+
+        $response->assertJsonFragment([
+            'title' => $book->title,
+        ]);
+    }
+
+    /** @test */
+    function can_create_books()
+    {
+        $response = $this->postJson(route('books.store'), [])
+            ->assertJsonValidationErrorFor('title');
+
+        $response = $this->postJson(route('books.store'), [
+            'title' => 'My new book',
+        ]);
+
+        $response->assertJsonFragment([
+            'title' => 'My new book',
+        ]);
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'My new book',
+        ]);
+    }
+
+    /** @test */
+    function can_update_books()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->patchJson(route('books.update', $book), [])
+            ->assertJsonValidationErrorFor('title');
+
+        $response = $this->patchJson(route('books.update', $book), [
+            'title' => 'Edited book',
+        ]);
+
+        $response->assertJsonFragment([
+            'title' => 'Edited book',
+        ]);
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'Edited book',
+        ]);
+    }
+
+    /** @test */
+    function can_delete_books()
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->deleteJson(route('books.destroy', $book));
+
+        $response->assertNoContent();
+
+        $this->assertDatabaseCount('books', 0);
+    }
+}
